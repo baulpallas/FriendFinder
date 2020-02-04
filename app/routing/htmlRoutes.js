b17efb89d9cc9eb5b08d5ee0ef6866b02364c7e9
Paul Ballas <paballas51@gmail.com>
2020-02-03 19:44:58 -0500

    html links

---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+var path = require("path");
+
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "../public/home.html"));
+});
+
+app.get("/survey", function(req, res) {
+  res.sendFile(path.join(__dirname, "../public/survey.html"));
+});
+
+app.listen(3000);
