08f7584de93eeff1715ed41696d270c16a96c49a
Paul Ballas <paballas51@gmail.com>
2020-02-03 22:54:32 -0500

    added some more mdl styling

---
+++ app/data/friends.js
@@ -0,0 +1,9 @@
+var db = [
+  {
+    name: "Mikey",
+    birthday: "07/04/1995",
+    scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+  }
+];
+
+exports.db = db;
--- server.js
+++ server.js
@@ -2,8 +2,4 @@ const path = require("path");
 const express = require("express");
 const app = express();
 
-app.get("/", function(req, res) {
-  res.send("Hello World");
-});
-
 app.listen(3000);
