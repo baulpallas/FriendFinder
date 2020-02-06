f7c766d883bed27869d12389b3388d62e8bb6ffd
Paul Ballas <paballas51@gmail.com>
2020-02-06 15:53:43 -0500

    add new feature for results page

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,6 +3,31 @@ var db = [
     name: "Mikey",
     birthday: "07/04/1995",
     scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+  },
+  {
+    name: "Russo",
+    birthday: "08/04/1992",
+    scores: [2, 10, 9, 2, 3, 8, 2, 3, 10, 10]
+  },
+  {
+    name: "Jackie",
+    birthday: "11/04/1997",
+    scores: [1, 10, 9, 8, 6, 7, 4, 3, 1, 10]
+  },
+  {
+    name: "Laura",
+    birthday: "10/04/1993",
+    scores: [10, 10, 10, 10, 10, 10, 10, 10, 10, 10]
+  },
+  {
+    name: "Mary",
+    birthday: "10/04/1992",
+    scores: [1, 1, 7, 8, 9, 1, 2, 3, 10, 2]
+  },
+  {
+    name: "Paul",
+    birthday: "12/21/1993",
+    scores: [5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
   }
 ];
 
